e3400273769fd3c8bd2a83dd3240abb2d466ae43
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2024-01-12 15:56:57 +0300

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,29 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = self.get_response_body()
+        converted_data = json.loads(response)
+        return converted_data
+
+URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
+CONVERTED_DATA = [
+    {'name': 'Daniel', 'occupation': 'LG Fridge Salesman'},
+    {'name': 'Joe', 'occupation': 'WiFi Fixer'},
+    {'name': 'Avi', 'occupation': 'DJ'},
+    {'name': 'Howard', 'occupation': 'Mountain Legend'}
+]
+
+def test_get_response():
+    '''get_response_body function returns response.'''
+    requester = GetRequester(URL)
+    assert requester.get_response_body() == JSON_STRING
+
+def test_load_json():
+    '''load_json function returns response.'''
+    requester = GetRequester(URL)
+    assert requester.load_json() == CONVERTED_DATA
